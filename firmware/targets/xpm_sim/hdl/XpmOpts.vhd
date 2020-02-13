84d798e0e821173f98404add081c1dee4bba374f
Matt Weaver <weaver@slac.stanford.edu>
2020-02-13 08:29:10 -0800

    Add xpm_sim target

---
+++ firmware/targets/xpm_sim/hdl/XpmOpts.vhd
@@ -0,0 +1,15 @@
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+library surf;
+use surf.StdRtlPkg.all;
+
+package XpmOpts is
+
+  constant TPGMINI_C : boolean := false;
+  constant XTPG_MODE           : sl := '1';
+  constant US_RX_ENABLE_INIT_C : sl := '1';
+  constant CU_RX_ENABLE_INIT_C : sl := '0';
+  
+end XpmOpts;
