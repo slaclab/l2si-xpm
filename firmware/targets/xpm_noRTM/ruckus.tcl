8a015141c1f4b5044f5e6df46a573eaed8357318
Matt Weaver <weaver@slac.stanford.edu>
2021-07-28 12:06:52 -0700

    Add constraints to target

--- firmware/targets/xpm_noRTM/ruckus.tcl
+++ firmware/targets/xpm_noRTM/ruckus.tcl
@@ -9,4 +9,5 @@ loadRuckusTcl $::env(TOP_DIR)/common/gthUltraScale
 
 # Load target's source code and constraints
 loadSource      -dir  "$::DIR_PATH/hdl/"
-loadConstraints -dir  $::env(TOP_DIR)/common/xpm/xdc
+loadConstraints -dir  "$::DIR_PATH/hdl/"
+#loadConstraints -dir  $::env(TOP_DIR)/common/xpm/xdc
