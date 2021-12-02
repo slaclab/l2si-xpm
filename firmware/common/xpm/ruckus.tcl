cdf1e3c22a45cc1945f3ee17be2cf96ee4bc418e
Matt Weaver <weaver@slac.stanford.edu>
2021-12-01 17:07:59 -0800

    Update xtpg target with syncmsg fix.

--- firmware/common/xpm/ruckus.tcl
+++ firmware/common/xpm/ruckus.tcl
@@ -3,3 +3,8 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
 loadSource -lib l2si -dir "$::DIR_PATH/rtl/"
+
+# Load Amc Common Carrier modules
+if { $::env(PRJ_PART) == "XCKU040-FFVA1156-2-E" } {
+   loadSource -lib l2si -dir "$::DIR_PATH/rtl/amcc"
+}
